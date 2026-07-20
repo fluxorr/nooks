23b17c23c8c62b29f4cba92043ebba3de5b95e9b
Rahul Chaudhari_ <rahulgc545@gmail.com>
2026-07-20 11:00:46 +0530

    fix: align Scales border-x with max-w-4xl container edges, use diagonal pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -337,11 +337,11 @@ export default function Page() {
           </div>
         </section>
 
-        <div className="fixed inset-y-0 left-1/2 -translate-x-[384px] w-6 pointer-events-none overflow-hidden opacity-[0.08] hidden lg:block">
-          <Scales size={8} orientation="vertical" />
+        <div className="fixed inset-y-0 left-1/2 -translate-x-[448px] w-6 pointer-events-none overflow-hidden opacity-[0.08] hidden lg:block">
+          <Scales size={8} />
         </div>
-        <div className="fixed inset-y-0 right-1/2 translate-x-[384px] w-6 pointer-events-none overflow-hidden opacity-[0.08] hidden lg:block">
-          <Scales size={8} orientation="vertical" />
+        <div className="fixed inset-y-0 right-1/2 translate-x-[448px] w-6 pointer-events-none overflow-hidden opacity-[0.08] hidden lg:block">
+          <Scales size={8} />
         </div>
 
         {/* CTA */}
